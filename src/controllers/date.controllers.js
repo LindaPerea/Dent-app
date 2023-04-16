d32f3d36570e1ae45c454b5ecb0f9848809ce05f
Oscar Tandioy <tato.tandioy@gmail.com>
2023-04-15 19:02:19 -0500

    feat: Created the path to get the appointments

--- src/controllers/date.controllers.js
+++ src/controllers/date.controllers.js
@@ -2,22 +2,52 @@ const Date = require("../models/Date");
 const catchError = require("../utils/catchError");
 
 const getAll = catchError(async (req, res) => {
-  const results = await Date.findAll();
-  console.log(results);
-  return res.json(results)
+  try {
+    const results = await Date.findAll();
+    return res.json(results)
+  } catch (error) {
+    throw error;
+  }
 });
 
 const create = catchError(async (req, res) => {
-  const { appointmentDate } = req.body;
-  const { user } = req;
-  const createDate = { appointmentDate, userId: user.id }
-  const result = Date.create(createDate);
-  if (!result) return res.status(401).json({ message: 'Fallo al crear la cita' })
-  return res.status(201).json(result)
+  try {
+    const { appointmentDate } = req.body;
+    const { user } = req;
+    const createDate = { appointmentDate, userId: user.id }
+    const result = Date.create(createDate);
+    if (!result) return res.status(401).json({ message: 'Fallo al crear la cita' })
+    return res.status(201).json({ message: 'Se creo con exito' })
+  } catch (error) {
+    throw error
+  }
 });
 
+const getById = catchError(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Date.findOne({ where: { id } });
+    if (!result) return res.status(404).json({ message: 'Not Found Date', id });
+    res.json(result);
+  } catch (error) {
+    throw error;
+  }
+});
+
+const getAppointments = catchError(async (req, res) => {
+  try {
+    const { userId } = req.body
+    const result = await Date.findOne({ where: { userId } });
+    if (!result) return res.status(404).json({ message: 'Not Found appointments', user: userId });
+    res.json(result)
+  } catch (error) {
+    throw error
+  }
+});
 
 module.exports = {
   getAll,
-  create
+  create,
+  getById,
+  getAppointments
 }
--- src/routes/date.routes.js
+++ src/routes/date.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getAll, create } = require("../controllers/date.controllers");
+const { getAll, create, getById, getAppointments } = require("../controllers/date.controllers");
 const verifyJWT = require('../utils/verifyJWT');
 const verifySchema = require('../schemas/joiSchema.checker');
 const { createDateSchema } = require("../schemas/date.schema");
@@ -10,5 +10,8 @@ const dateRouter = Router();
 dateRouter.route('/')
   .get(getAll)
   .post(verifySchema(createDateSchema, 'body'), verifyJWT, create)
-
+dateRouter.route('/appointments')
+  .get(getAppointments)
+dateRouter.route('/:id')
+  .get(getById)
 module.exports = dateRouter;
