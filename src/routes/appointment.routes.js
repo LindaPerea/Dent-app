633191fb40f0fc9cdfb09582777cd3742131c0c0
LindaPerea <linda.hp@hotmail.com>
2023-04-16 21:29:58 -0500

    modificando names routes of date to appointment

--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
-const { getAll, create, getById, getAppointments, deleteById } = require("../controllers/date.controllers");
+const { getAll, create, getById, getAppointments, deleteById } = require("../controllers/appointment.controllers");
 const verifyJWT = require('../utils/verifyJWT');
 const verifySchema = require('../schemas/joiSchema.checker');
-const { createAppointmentSchema } = require("../schemas/date.schema");
+const { createAppointmentSchema } = require("../schemas/appointment.schema");
 const { verifyAdministrator } = require("../middlewares/VerifyAdministrator");
 const { verifyIsTheSameUser } = require("../middlewares/verifyIsTheSameUser");
 
-const dateRouter = Router();
+const appointmentRouter = Router();
 
 // For new routes the user id must be placed in params as userId
-dateRouter.route('/')
+appointmentRouter.route('/')
   .get(verifyJWT, verifyAdministrator, getAll)
   .post(verifySchema(createAppointmentSchema, 'body'), verifyJWT, create)
-dateRouter.route('/appointments')
+appointmentRouter.route('/appointments')
   .get(verifyJWT, verifyIsTheSameUser, verifyAdministrator, getAppointments)
-dateRouter.route('/:id')
+appointmentRouter.route('/:id')
   .get(verifyJWT, verifyIsTheSameUser, verifyAdministrator, getById)
   .delete(verifyJWT, verifyIsTheSameUser, verifyAdministrator, deleteById)
-module.exports = dateRouter;
+module.exports = appointmentRouter;
