960f0b7eb18adb54b54335c510028104f6cf7b52
LindaPerea <linda.hp@hotmail.com>
2023-04-17 18:19:41 -0500

    modify for render

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize({
+  // process.env.DATABASE_URL
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
 
 module.exports = sequelize;
